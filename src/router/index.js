657393d513f0c4577791d7f9da075eaa628e48ab
test <test@qq.com>
2018-01-16 18:13:41 +0800

    'x'

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/components/Layout'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path:'/',
+      name:'Layout',
+      component:Layout
+    }
+  ]
+})
